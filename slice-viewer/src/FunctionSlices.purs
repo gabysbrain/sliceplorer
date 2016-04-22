3e8cf742a5263e8dcbd1ba945129baa0ba13f9ca
Thomas Torsney-Weir <torsneyt@gmail.com>
2016-04-22 18:11:25 +0200

    auto load data

--- slice-viewer/src/FunctionSlices.purs
+++ slice-viewer/src/FunctionSlices.purs
@@ -2,34 +2,41 @@ module App.FunctionSlices where
 
 import Prelude 
 import Data.Maybe (Maybe(Just, Nothing))
+import Data.Maybe.Unsafe (fromJust)
 import Data.Either (Either(..), either)
 import Data.Either.Unsafe (fromRight)
-import Data.Array (length)
+import Data.Array (length, (..))
+import Data.Int (fromString)
 
 import Control.Monad.Aff (attempt)
 import Control.Monad.Eff.Exception (Error)
 import Network.HTTP.Affjax (AJAX, get)
 
 import Pux (EffModel, noEffects)
-import Pux.Html (Html, div, button, input, text, p)
-import Pux.Html.Events (onChange, onClick)
+import Pux.Html (Html, div, span, button, input, text, p, select, option)
+import Pux.Html.Events (onChange, onClick, FormEvent)
+import Pux.Html.Attributes (className, selected, value)
 
 import Data.Samples (SampleGroup(..), parse)
 
 type State =
   { d :: Int
   , function :: String
-  , errorMsg :: String
+  , error :: Maybe Error
   , samples :: Maybe SampleGroup
   }
 
-data Action = RequestSamples | ReceiveSamples (Either Error SampleGroup)
+data Action 
+  = RequestSamples 
+  | ReceiveSamples (Either Error SampleGroup)
+  | DimChange FormEvent
+  | FunctionChange FormEvent
 
 init :: State
 init = 
   { d: 2
   , function: "spherical"
-  , errorMsg: ""
+  , error: Nothing
   , samples: Nothing
   }
 
@@ -37,24 +44,68 @@ update :: Action -> State -> EffModel State Action (ajax :: AJAX)
 update (RequestSamples) state =
   { state: state {samples=Nothing}
   , effects: [ do
-      --res <- attempt $ get "/data/spherical_2_slices.csv"
-      res <- attempt $ get "/data/test.csv"
+      let url = "/data/" ++ state.function ++ "_" ++ (show state.d) ++ "_slices.csv"
+      --res <- attempt $ get "/data/test.csv"
+      res <- attempt $ get url
       --x = fromRight res
       let samples = either Left (\r -> Right $ parse r.response) res
       return $ ReceiveSamples samples
     ]
   }
 update (ReceiveSamples (Left err)) state =
-  noEffects $ state {errorMsg = show err}
+  noEffects $ state {error = Just err}
 update (ReceiveSamples (Right s)) state =
-  noEffects $ state {samples = Just s}
+  noEffects $ state {samples = Just s, error = Nothing}
+update (DimChange ev) state = 
+  -- TODO: maybe some error checking?
+  { state: state {d=fromJust $ fromString ev.target.value}
+  , effects: [ do
+      return RequestSamples
+    ]
+  }
+update (FunctionChange ev) state = 
+  -- TODO: maybe some error checking?
+  { state: state {function=ev.target.value}
+  , effects: [ do
+      return RequestSamples
+    ]
+  }
+
 
 view :: State -> Html Action
 view state = 
   div []
-    [ button [onClick (const RequestSamples)] [text "Fetch samples"]
-    , p [] [ case state.samples of
-               Nothing -> text "Nothing loaded"
-               Just (SampleGroup s)  -> text (show (length s)) ]
+    [ viewControls state
+    , viewError state.error
+    , p [] [text $ show state.d]
+    , p [] [text state.function]
+    , viewSamples state.samples
     ]
+
+viewControls :: State -> Html Action
+viewControls state = 
+  div [] 
+    [ dimSelector state.d
+    , funcSelector state.function
+    , button [onClick (const RequestSamples)] [text "Fetch samples"]
+    ]
+
+dimSelector :: Int -> Html Action
+dimSelector d =
+  select [onChange DimChange] $
+    map (\i -> option [value (show i), selected (i==d)] [text $ show i]) (2..10)
+
+funcSelector :: String -> Html Action
+funcSelector fname =
+  select [onChange FunctionChange] $
+    map (\f -> option [value f, selected (f==fname)] [text f])
+      ["ackley", "rosenbrock", "spherical", "schwefel", "zakharov"]
+
+viewError :: Maybe Error -> Html Action
+viewError Nothing  = text ""
+viewError (Just err) = span [className "error"] [text $ show err]
+
+viewSamples :: Maybe SampleGroup -> Html Action
+viewSamples Nothing = p [] [text "Nothing loaded"]
+viewSamples (Just (SampleGroup s)) = p [] [text (show (length s))]
 
