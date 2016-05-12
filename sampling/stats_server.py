c93ccc23abd4b0a3e525455391b11367336ccf34
Thomas Torsney-Weir <torsneyt@gmail.com>
2016-05-12 15:08:40 +0200

    only send top 1000 samples up

--- sampling/stats_server.py
+++ sampling/stats_server.py
@@ -16,7 +16,7 @@ class MyEncoder(json.JSONEncoder):
 @app.route('/slice/<function>/<int:dims>', methods = ['GET'])
 def slice_req(function, dims):
   s = convert_slices(function, dims)
-  resp = Response(response=json.dumps(list(itertools.islice(s, 500)), cls=MyEncoder),
+  resp = Response(response=json.dumps(list(itertools.islice(s, 1000)), cls=MyEncoder),
       status=200, mimetype="application/json")
   resp.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
   return resp
