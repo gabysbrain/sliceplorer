8a08c2c310ee6002b74c92cd87e2699a2181e654
Thomas Torsney-Weir <torsneyt@gmail.com>
2016-05-19 11:08:17 +0200

    subsample the datasets using sobol

--- sampling/translate_raw.py
+++ sampling/translate_raw.py
@@ -1,5 +1,7 @@
 
 from sys import argv
+import numpy as np
+from ortho_slices import sobol
 
 def data_sizes(fname):
   tmp = fname.split('.')
@@ -25,15 +27,46 @@ def slice_func(xsize, ysize, zsize, data):
     return slices
   return slice
 
+def sobol_samples(n, xsize, ysize, zsize):
+  vals = sobol(n, 3, 0)
+  vals[:,0] = np.interp(vals[:,0], [0,1], [0, xsize-1])
+  vals[:,1] = np.interp(vals[:,1], [0,1], [0, ysize-1])
+  vals[:,2] = np.interp(vals[:,2], [0,1], [0, zsize-1])
+  #print(max(vals))
+  vals = np.intc(np.round(vals))
+  # FIXME: there's a rounding problem here...
+  return vals
+
 if __name__ == '__main__':
-  xsize, ysize, zsize = data_sizes(argv[1])
+  xsize, ysize, zsize = data_sizes(argv[2])
   #print("x1,x2,x3,y1")
-  with open(argv[1], "rb") as file:
-    slices = slice_func(xsize, ysize, zsize, file)
-    for x in range(xsize):
-      for y in range(x, ysize):
-        for z in range(y, zsize):
-          for d in range(3):
-            for s in slices(d, x, y, z):
-              print("%s,%s,%s,%s" % s)
+  sample_n = int(argv[1])
+  with open(argv[2], "rb") as file:
+    get_slice = slice_func(xsize, ysize, zsize, file)
+    for [x,y,z] in sobol_samples(sample_n, xsize, ysize, zsize):
+      for d in range(3):
+        for s in get_slice(d, x, y, z):
+          print("%s,%s,%s,%s" % s)
+    # for x in range(xsize): # there are quite a few repeats in the slices this way
+      # for y in range(ysize):
+        # for z in range(zsize):
+          # for d in range(3):
+            # for s in slices(d, x, y, z):
+              # print("%s,%s,%s,%s" % s)
+
+    # x slices
+    # for y in range(ysize):
+      # for z in range(zsize):
+        # for s in slices(0, 0, y, z):
+          # print("%s,%s,%s,%s" % s)
+    # # y slices
+    # for x in range(xsize):
+      # for z in range(zsize):
+        # for s in slices(0, 0, y, z):
+          # print("%s,%s,%s,%s" % s)
+    # # z slices
+    # for x in range(xsize): # there are quite a few repeats in the slices this way
+      # for y in range(ysize):
+        # for s in slices(0, 0, y, z):
+          # print("%s,%s,%s,%s" % s)
 
