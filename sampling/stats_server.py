c88e478c7164ca387294b29b0579aa6aaba621a2
Thomas Torsney-Weir <torsneyt@gmail.com>
2016-06-02 07:20:01 +0200

    slice server returns list of datasets

--- sampling/stats_server.py
+++ sampling/stats_server.py
@@ -5,6 +5,7 @@ import itertools
 from compute_stats import convert_slices
 from cluster_slices import identify_clusters
 from slice_neighbors import slice_neighbors
+from slice_list import slice_list
 
 app = Flask(__name__)
 
@@ -14,6 +15,13 @@ class MyEncoder(json.JSONEncoder):
       return o.__dict__
     except AttributeError:
       return str(o)
+
+@app.route('/slice', methods = ['GET'])
+def slices():
+  s = [{'dataset': ds, 'dims': d} for ds,d in slice_list()]
+  resp = Response(response=json.dumps(s, cls=MyEncoder),
+      status=200, mimetype="application/json")
+  return resp
 
 @app.route('/slice/<function>/<int:dims>/<int:limit>', methods = ['GET'])
 def slice_req(function, dims, limit):
