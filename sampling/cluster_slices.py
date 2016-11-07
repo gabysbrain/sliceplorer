f70f5f2cf6ea2f182b8ccd761bfbff80b5c1147a
Thomas Torsney-Weir <torsneyt@gmail.com>
2016-11-07 04:03:18 -0500

    code to cluster slices across dimensions

--- sampling/cluster_slices.py
+++ sampling/cluster_slices.py
@@ -2,10 +2,14 @@
 import numpy as np
 from scipy.cluster import vq
 
-def cluster_slices(focus_points):
+def cluster_slices_across_dims(focus_points):
+  """ Cluster the slices across dimensions (ignoring dimension)
+  """
   focus_points = list(focus_points) # the generator will break things
   dims = focus_points[0].dims
   nsamples = focus_points[0].num_slice_samples
+  # cluster based on the input values as well as the outputs
+  # this is a matrix where each row is a 1D slice
   obs = np.zeros((len(focus_points)*dims, nsamples))
   for i,fp in enumerate(focus_points):
     for d, s in enumerate(fp.slices):
@@ -16,12 +20,33 @@ def cluster_slices(focus_points):
   cluster_ids,_ = vq.vq(wobs, centroids)
   return cluster_ids
 
-def identify_clusters(focus_points):
+def cluster_slices_within_dims(focus_points):
+  """ Cluster the slices within dimensions (separate clusters per dimension)
+  """
   focus_points = list(focus_points) # the generator will break things
   dims = focus_points[0].dims
-  cluster_ids = cluster_slices(focus_points)
+  nsamples = focus_points[0].num_slice_samples
+  # cluster based on the input values as well as the outputs
+  # this is list of matrice where each row is a 1D slice
+  dim_obs = [np.zeros((len(focus_points), nsamples))] * dims
+  cluster_ids = []
+  for i,fp in enumerate(focus_points):
+    for d,(s,obs) in enumerate(zip(fp.slices, dim_obs)):
+      for j,x in enumerate(s.slice):
+        obs[i, j] = x['y']
+  # now cluster each dimension individually
+  for obs in dim_obs:
+    wobs = vq.whiten(obs) # make everything uniform
+    centroids,_ = vq.kmeans(wobs, 6)
+    cids,_ = vq.vq(wobs, centroids)
+    cluster_ids.append(cids)
+  return cluster_ids # this is an array of cluster ids per dimension
+
+def identify_clusters(focus_points):
+  focus_points = list(focus_points) # the generator will break things
+  cluster_ids = cluster_slices_within_dims(focus_points)
   for i,fp in enumerate(focus_points):
     for d, s in enumerate(fp.slices):
-      s.cluster_id = int(cluster_ids[i*dims+d])
+      s.cluster_id = int(cluster_ids[d][i])
   return focus_points
 
