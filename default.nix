b8fffb60c44ceac85fb1d645df1f7e2bae9bf9a4
Thomas <torsnet6cs@sliceplorer.cs.univie.ac.at>
2016-11-16 14:43:05 +0100

    basic nix shell incorporating required python modules

---
+++ default.nix
@@ -0,0 +1,33 @@
+with import <nixpkgs> {}; 
+
+let
+  mypypi = import ./mypypi.nix {
+    pkgs = pkgs;
+    python = python35Packages;
+  };
+
+in
+{
+  devEnv = stdenv.mkDerivation {
+    name = "sliceplorer-dev";
+    buildInputs = [
+      pkgconfig freetype
+      nodejs
+      haskellPackages.purescript
+
+      pypi2nix
+      npm2nix
+      nodePackages.bower2nix
+
+      python3
+      python3Packages.matplotlib
+      python3Packages.pandas
+      python3Packages.numpy
+      python3Packages.scipy
+      python3Packages.flask
+      mypypi.rpy2
+      mypypi.sobol-seq
+    ];
+  };
+}
+
