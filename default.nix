cb1bb5e4968ca8bf3cd8d2322c0677603497c9d0
Thomas Torsney-Weir <thomas.torsney-weir@univie.ac.at>
2016-11-16 17:06:04 +0100

    more development pacakges

--- default.nix
+++ default.nix
@@ -11,15 +11,18 @@ in
   devEnv = stdenv.mkDerivation {
     name = "sliceplorer-dev";
     buildInputs = [
-      pkgconfig freetype
-      nodejs
+      pkgconfig freetype cairo libjpeg libpng
+      nodejs-4_x
       haskellPackages.purescript
+      silver-searcher
 
       pypi2nix
-      npm2nix
+      #npm2nix
+      nodePackages.node2nix
       nodePackages.bower2nix
 
       python3
+      python3Packages.gunicorn
       python3Packages.matplotlib
       python3Packages.pandas
       python3Packages.numpy
