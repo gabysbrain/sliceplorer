d1162f4b3ac6b7839a7f79fe47b1b01c44245dd7
Thomas Torsney-Weir <torsneyt@gmail.com>
2017-02-05 22:00:34 +0100

    typo

--- slice-viewer/src/Data/Config.js
+++ slice-viewer/src/Data/Config.js
@@ -6,6 +6,6 @@ var devMode = process.env.NODE_ENV === 'development';
 var devServer = 'http://localhost:5000';
 var prodServer = 'http://sliceplore.cs.univie.ac.at';
 
-exports.server = devMode ? devServer : prodserver;
+exports.server = devMode ? devServer : prodServer;
 
 
