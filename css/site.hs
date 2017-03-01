e2f026e15bdf44f3b0f2823f6629ebb77f1c6c8b
Thomas Torsney-Weir <torsneyt@gmail.com>
2017-03-01 19:16:05 +0100

    update styling

--- css/site.hs
+++ css/site.hs
@@ -5,8 +5,21 @@ import Data.Monoid ((<>))
 
 site :: Css
 site = do
+  overviewPage
+  exPage
+
+overviewPage :: Css
+overviewPage = do
+  th # ".technique" ?
+    do transform $ rotate (deg 315)
+  star # "#table-section" ?
+    do table ?
+         do fontSize (pct 60.0)
+
+exPage :: Css
+exPage = do
   star # ".task-example" ?
-    do h1 <> h2 ?
+    do h2 <> h3 ?
          do textAlign (alignSide sideCenter)
        ul ?
          do listStyleType none
