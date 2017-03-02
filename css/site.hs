5e36bbcc726668b728ffa7f2d4db3bf9fc0ad0af
Thomas Torsney-Weir <torsneyt@gmail.com>
2017-03-02 14:20:31 +0100

    table styling

--- css/site.hs
+++ css/site.hs
@@ -1,6 +1,7 @@
  
 {-# LANGUAGE OverloadedStrings #-}
 import Clay
+import Prelude hiding ((**))
 import Data.Monoid ((<>))
 
 site :: Css
@@ -10,11 +11,34 @@ site = do
 
 overviewPage :: Css
 overviewPage = do
+  th # ".task" ?
+    do width (pct 20.0)
+  th # ".description" ?
+    do width (pct 40.0)
   th # ".technique" ?
     do transform $ rotate (deg 315)
+       width (px 20)
+       overflow visible
   star # "#table-section" ?
     do table ?
          do fontSize (pct 60.0)
+            "table-layout" -: "fixed"
+       td ?
+         do height (pct 100.0)
+       td ** a # ".color-code" ?
+         do height (pct 100.0)
+            display block
+            lineHeight (px 0)
+            fontSize (px 0)
+            color transparent
+       star # ".color-code" # ".code-0" ?
+         do backgroundColor (rgb 254 235 226)
+       star # ".color-code" # ".code-1" ?
+         do backgroundColor (rgb 251 180 185)
+       star # ".color-code" # ".code-2" ?
+         do backgroundColor (rgb 247 104 161)
+       star # ".color-code" # ".code-3" ?
+         do backgroundColor (rgb 174   1 126)
 
 exPage :: Css
 exPage = do
@@ -23,7 +47,6 @@ exPage = do
          do textAlign (alignSide sideCenter)
        ul ?
          do listStyleType none
-            --border solid (px 1) grey
 
 main :: IO ()
 main = putCss site
